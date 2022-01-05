fae40bf92c0aec8b63b98f4e9767102e7de6264b
SeifOSallam <93483227+SeifOSallam@users.noreply.github.com>
2022-01-05 21:41:01 +0200

    Update web-request.service.ts

--- frontend/src/app/web-request.service.ts
+++ frontend/src/app/web-request.service.ts
@@ -10,7 +10,7 @@ export class WebRequestService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
-    this.ROOT_URL = 'http://backend-todo1.apps.eu45.prod.nextcle.com/';
+    this.ROOT_URL = '10.130.3.203:8080';
   }
 
   get(uri: string) {
