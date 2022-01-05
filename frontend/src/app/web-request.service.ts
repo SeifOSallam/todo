4c59b85cc72f722d3745116bbeca2c538c18d1e2
Mostafa Bekhet <71971114+MostafaBekhet@users.noreply.github.com>
2022-01-05 21:21:31 +0200

    Update web-request.service.ts

--- frontend/src/app/web-request.service.ts
+++ frontend/src/app/web-request.service.ts
@@ -10,7 +10,7 @@ export class WebRequestService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
-    this.ROOT_URL = 'http://127.0.0.1:8080';
+    this.ROOT_URL = 'http://backend-todo1.apps.eu45.prod.nextcle.com/';
   }
 
   get(uri: string) {
